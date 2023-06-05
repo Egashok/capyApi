cc953efea87da3a3bd04359a90a20e0ff11b2bf7
egashok <55044715+Egashok@users.noreply.github.com>
2023-06-05 16:03:26 +0600

    ye

--- services/capys.py
+++ services/capys.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 import models
 import schemas
 
-def add_capy(db: Session, capy: schemas.CapyToCreate)->models.CapyInDB:
+def add_capy(db: Session, capy: schemas.CapyToCreate):
     db_capy = models.CapyInDB(
         name=capy.name,
         description=capy.description,
@@ -15,22 +15,22 @@ def add_capy(db: Session, capy: schemas.CapyToCreate)->models.CapyInDB:
     db.refresh(db_capy)
     return db_capy
 
-def get_capy_by_id(db: Session, capy_id: int)->models.CapyInDB:
+def get_capy_by_id(db: Session, capy_id: int):
     capy = db.query(models.CapyInDB).filter(
         models.CapyInDB.id == capy_id
     ).first()
     return capy
 
-def get_img_by_id(db: Session, capy_id: int)->models.CapyInDB.photo:
+def get_img_by_id(db: Session, capy_id: int):
     capy = db.query(models.CapyInDB).filter(
         models.CapyInDB.id == capy_id
     ).first()
     return capy.photo
 
-def get_capys(db: Session)->models.CapyInDB:
+def get_capys(db: Session):
     return db.query(models.CapyInDB).all()
 
-def update_capy(db: Session, capy_id: int, new_capy: schemas.CapyToCreate)->models.CapyInDB:
+def update_capy(db: Session, capy_id: int, new_capy: schemas.CapyToCreate):
     capy = get_capy_by_id(db, capy_id)
     capy.name = new_capy.name
     capy.description=new_capy.description
@@ -46,11 +46,3 @@ def delete_capy(db: Session, capy_id: int):
     db.delete(capy)
     db.commit()
     return None 
-
-
-
-
-
-
-
-
